ad069dac1a9a5905595d020a0d71e3abdd726668
Xvaria <Xvongola23@gmail.com>
2021-10-18 15:45:08 -0500

    Ready | Basic annotations - floor

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 '''Basic annotations'''
+import math
 
 
 def floor(n: float) -> int:
     '''Returns the floor of the float'''
-    return round(n)
+    return math.floor(n)
