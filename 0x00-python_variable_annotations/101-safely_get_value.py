9bf77154c2b55737b50c97b1d30adf88caefb7b6
Xvaria <Xvongola23@gmail.com>
2021-10-18 18:00:25 -0500

    Ready | More involved type annotations

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''More involved type annotations'''
+from typing import Mapping, Any, Union, TypeVar
+
+t = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[t, None] = None) -> Union[Any, t]:
+    '''Return values, add type annotations to the function'''
+    if key in dct:
+        return dct[key]
+    else:
+        return default
