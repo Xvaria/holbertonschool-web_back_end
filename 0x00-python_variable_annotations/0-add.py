0746e06c0b9cc2d3bd025341a035c6b8d91d22d3
Xvaria <Xvongola23@gmail.com>
2021-10-18 15:40:20 -0500

    Ready | Basic annotations - add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-'''Function add a and b arguments and returns their sum as a float'''
+'''Basic annotations'''
 
 
 def add(a: float, b: float) -> float:
+    '''Function add a and b arguments and returns their sum as a float'''
     return a + b
